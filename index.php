ce759f4dc52049c155395ac5be2aff9408225c5e
acheroptonio-dicabrito <acheroptonio@gmail.com>
2020-06-30 03:06:47 -0300

    add pageAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \ebuai\Page;
+use \ebuai\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/optimus_prime', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
