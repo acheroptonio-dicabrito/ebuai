98fd3615b509fc9e613f1602d62d7ad3ce456730
acheroptonio-dicabrito <acheroptonio@gmail.com>
2020-07-04 16:32:51 -0300

    add user CRUD

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ $app->get('/', function() {
 //building page admin
 $app->get('/admin', function() {
 
+	//verify user session
 	User::verifyLogin();
     
 	$page = new PageAdmin();
@@ -49,9 +50,10 @@ $app->get('/admin/login', function(){
 
 });
 
-//function login
+//route login panel admin
 $app->post('/admin/login', function(){
 
+	//verify user session
 	User::login($_POST['login'], $_POST['password']);
 
 	header('Location: /admin');
@@ -60,14 +62,122 @@ $app->post('/admin/login', function(){
 
 });
 
+//route logout panel admin
 $app->get('/admin/logout', function() {
 
+	//set null user session
 	User::logout();
 
 	header('Location: /admin/login');
 	exit;
 
 });
+
+//route list users panel admin
+$app->get('/admin/users', function(){
+
+	//verify user session
+	User::verifyLogin();
+
+	$users = User::listAll();
+    
+	$page = new PageAdmin();
+
+	$page->setTpl('users', array(
+		'users'=>$users
+	));
+
+});
+
+//route page users create panel admin
+$app->get('/admin/users/create', function(){
+
+	//verify user session
+	User::verifyLogin();
+    
+	$page = new PageAdmin();
+
+	$page->setTpl('users-create');
+
+});
+
+//route delete users panel admin
+$app->get('/admin/users/:iduser/delete', function($iduser){
+
+	//verify user session
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header('Location: /admin/users');
+	exit;
+
+});
+
+//route page users update panel admin
+$app->get('/admin/users/:iduser', function($iduser){
+
+	//verify user session
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+    
+	$page = new PageAdmin();
+
+	$page->setTpl('users-update', array(
+		'user'=>$user->getValues()
+	));
+
+});
+
+//route create new user panel admin
+$app->post('/admin/users/create', function(){
+
+	//verify user session
+	User::verifyLogin();
+
+	$user = new User();
+
+	//verify user level
+	$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header('Location: /admin/users');
+	exit;
+
+});
+
+//route page update user panel admin
+$app->post('/admin/users/:iduser', function($iduser){
+
+	//verify user session
+	User::verifyLogin();
+
+	$user = new User();
+
+	//verify user level
+	$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header('Location: /admin/users');
+	exit;
+
+});
+
 
 $app->run();
 
