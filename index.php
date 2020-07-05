ccca2ebca7002dbc8a916008667041eaea6c3ea2
acheroptonio-dicabrito <acheroptonio@gmail.com>
2020-07-05 16:52:01 -0300

    add forgot password

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ $app->get('/admin', function() {
 $app->get('/admin/login', function(){
 
 	$page = new PageAdmin([
+		//disable header footer page login
 		'header'=>false,	
 		'footer'=>false	
 	]);
@@ -178,6 +179,81 @@ $app->post('/admin/users/:iduser', function($iduser){
 
 });
 
+$app->get('/admin/forgot', function() {
+
+	$page = new PageAdmin([
+		//disable header footer page forgot
+		'header'=>false,	
+		'footer'=>false	
+	]);
+
+	$page->setTpl('forgot');
+
+});
+
+$app->post('/admin/forgot', function() {
+
+	$user = User::getForgot($_POST['email']);
+
+	header('Location: /admin/forgot/sent');
+	exit;
+
+});
+
+$app->get('/admin/forgot/sent', function() {
+
+	$page = new PageAdmin([
+		//disable header footer page forgot sent
+		'header'=>false,	
+		'footer'=>false	
+	]);
+
+	$page->setTpl('forgot-sent');
+
+});
+
+$app->get('/admin/forgot/reset', function() {
+
+	$user = User::validForgotDecrypt($_GET['code']);
+
+	$page = new PageAdmin([
+		//disable header footer page forgot reset
+		'header'=>false,	
+		'footer'=>false	
+	]);
+
+	$page->setTpl('forgot-reset', array(
+		'name'=>$user['desperson'],
+		'code'=>$_GET['code']
+	));	
+
+});
+
+$app->post('/admin/forgot/reset', function() {
+
+	$forgot = User::validForgotDecrypt($_POST['code']);
+
+	User::setForgotUsed($forgot['idrecovery']);
+
+	$user = new User();
+
+	$user->get((int)$forgot['iduser']);
+
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT, [
+		'cost' => 12,
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		//disable header footer page forgot success
+		'header'=>false,	
+		'footer'=>false	
+	]);
+
+	$page->setTpl('forgot-reset-success');	
+
+});
 
 $app->run();
 
--- vendor/ebuai/php-classes/src/Model/User.php
+++ vendor/ebuai/php-classes/src/Model/User.php
@@ -4,13 +4,17 @@ namespace ebuai\Model;
 
 //calling sql class
 use \ebuai\DB\Sql;
-//calling sql class
+//calling model class
 use \ebuai\Model;
+//calling mailer class
+use \ebuai\Mailer;
 
 //model user class
 class User extends Model{
 
 	const SESSION = 'User';
+	const SECRET = 'ebuaiPhp7_Secret';
+	const SECRET_IV = 'ebuaiPhp7_Secret_IV';
 
 	//verifying login
 	public static function login($login, $password) {
@@ -78,6 +82,16 @@ class User extends Model{
 
 	}
 
+	//function hash password user
+	public static function getPasswordHash($password)
+	{
+
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+		]);
+
+	}
+
 	//function create an user
 	public function save() {
 
@@ -86,7 +100,7 @@ class User extends Model{
 		$results = $sql->select('CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)', array(
 			':desperson'=>$this->getdesperson(),
 			':deslogin'=>$this->getdeslogin(),
-			':despassword'=>$this->getdespassword(),
+			':despassword'=>User::getPasswordHash($this->getdespassword()),
 			':desemail'=>$this->getdesemail(),
 			':nrphone'=>$this->getnrphone(),
 			':inadmin'=>$this->getinadmin()
@@ -118,7 +132,7 @@ class User extends Model{
 			':iduser'=>$this->getiduser(),
 			':desperson'=>$this->getdesperson(),
 			':deslogin'=>$this->getdeslogin(),
-			':despassword'=>$this->getdespassword(),
+			':despassword'=>User::getPasswordHash($this->getdespassword()),
 			':desemail'=>$this->getdesemail(),
 			':nrphone'=>$this->getnrphone(),
 			':inadmin'=>$this->getinadmin()
@@ -134,6 +148,130 @@ class User extends Model{
 		$sql = new Sql();
 
 		$results = $sql->query('CALL sp_users_delete(:iduser)', array(
+			':iduser'=>$this->getiduser()
+		));
+
+	}
+
+	public static function getForgot($email, $inadmin = true) {
+
+		$sql = new Sql();
+
+		$results = $sql->select('
+					SELECT * 
+					FROM tb_persons a 
+					INNER JOIN tb_users b 
+					USING(idperson) 
+					WHERE a.desemail = :email;', 
+					array(
+						':email'=>$email
+					));
+
+		if (count($results) === 0) {
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+			
+		}
+		else {
+
+			$data = $results[0];
+
+			$resultsRecovery = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser"=>$data['iduser'],
+				":desip"=>$_SERVER['REMOTE_ADDR']
+			));
+
+			if (count($resultsRecovery) === 0) {
+
+				throw new \Exception("Não foi possível recuperar a senha.");
+				
+			}
+
+			else {
+
+				$dataRecovery = $resultsRecovery[0];
+
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				if ($inadmin === true) {
+
+					$link = "http://www.ebuai.com.br/admin/forgot/reset?code=$code";
+
+				} else {
+
+					$link = "http://www.ebuai.com.br/forgot/reset?code=$code";
+					
+				}				
+
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da ebuai", "forgot", array(
+					"name"=>$data['desperson'],
+					"link"=>$link
+				));				
+
+				$mailer->send();
+
+				return $link;
+
+			}
+
+		}
+
+	}
+
+	public static function validForgotDecrypt($code) {
+
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack('a16', User::SECRET), 0, pack('a16', User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select('
+			SELECT *
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+			a.idrecovery = :idrecovery
+			AND
+			a.dtrecovery IS NULL
+			AND
+			DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();',
+			array(
+				':idrecovery'=>$idrecovery
+			));
+		if (count($results) === 0) {
+
+			throw new \Exception("Não foi possível recuperar a senha");
+			
+		}
+
+		else {
+
+			return $results[0];
+
+		}
+
+	}
+
+	public static function setForgotUsed($idrecovery) {
+
+		$sql = new Sql();
+
+		$sql->query('UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery', array(
+			':idrecovery'=>$idrecovery
+		));
+
+	}
+
+	public function setPassword($password) {
+
+		$sql = new Sql();
+
+		$sql->query('UPDATE tb_users SET despassword = :password WHERE iduser = :iduser', array(
+			':password'=>$password,
 			':iduser'=>$this->getiduser()
 		));
 
