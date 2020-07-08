4d2cbe4b5531c08776238f10329888494395950c
acheroptonio-dicabrito <acheroptonio@gmail.com>
2020-07-08 14:22:45 -0300

    Admin Categories

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ use \ebuai\Page;
 use \ebuai\PageAdmin;
 //calling user class
 use \ebuai\Model\User;
+//calling category class
+use \ebuai\Model\Category;
 
 $app = new Slim();
 
@@ -252,6 +254,99 @@ $app->post('/admin/forgot/reset', function() {
 	]);
 
 	$page->setTpl('forgot-reset-success');	
+
+});
+
+$app->get('/admin/categories', function() {
+
+	//verify user session
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('categories', [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get('/admin/categories/create', function() {
+
+	//verify user session
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('categories-create');
+
+});
+
+$app->post('/admin/categories/create', function() {
+
+	//verify user session
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+
+	//verify user session
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+
+	//verify user session
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl('categories-update', [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) {
+
+	//verify user session
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
 
 });
 
