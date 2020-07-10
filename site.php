37d95bd6f9c82af86f78506e1d4bddcb8ddb5f7f
acheroptonio-dicabrito <acheroptonio@gmail.com>
2020-07-10 02:31:04 -0300

    Pagination

--- site.php
+++ site.php
@@ -1,8 +1,8 @@
 <?php 
 
 use \ebuai\Page;
-use \ebuai\Model\Category; 
 use \ebuai\Model\Product; 
+use \ebuai\Model\Category; 
 
 //building page main
 $app->get('/', function() {
@@ -20,19 +20,31 @@ $app->get('/', function() {
 //building page category
 $app->get('/categories/:idcategory', function($idcategory) {
 
+	$page = (isset($_GET['page']))?(int)$_GET['page']:1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
 
 	$page = new Page();
 
 	$page->setTpl('category', [
 		'category'=>$category->getValues(),
-		'products'=>Product::checkList($category->getProducts())
+		'products'=>$pagination['data'],
+		'pages'=>$pages
 	]);
 
 });
-
-
 
 ?>
